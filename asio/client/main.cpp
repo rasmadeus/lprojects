b8be0699657b42b0289954a57faa68391fb0eaa6
K. Kulikov <rasmadeus@gmail.com>
2017-02-26 21:18:14 +0300

    - CS example.

--- asio/client/main.cpp
+++ asio/client/main.cpp
@@ -1,3 +1,29 @@
+void write_to_socket(boost::asio::ip::tcp::socket& sock)
+{
+    const std::string buf{"Hello world!"};
+    std::size_t bytes_written = 0;
+    while(bytes_written != buf.length())
+    {
+        const auto data = boost::asio::buffer(buf.c_str() + bytes_written, buf.length() - bytes_written);
+        bytes_written += sock.write_some(data);
+    }
+}
+
 int main()
 {
+    const std::string address{"127.0.0.1"};
+    const short port = 3333;
+
+    try
+    {
+        boost::asio::ip::tcp::endpoint ep{boost::asio::ip::address::from_string(address), port};
+        boost::asio::io_service ios;
+        boost::asio::ip::tcp::socket sock{ios, ep.protocol()};
+        sock.connect(ep);
+        write_to_socket(sock);
+    }
+    catch(const boost::system::system_error& ex)
+    {
+        std::cerr << "Error: " << ex.code() << ". " << ex.what() << std::endl;
+    }
 }
--- asio/server/main.cpp
+++ asio/server/main.cpp
@@ -1,7 +1,33 @@
+std::string read_from(boost::asio::ip::tcp::socket& sock)
+{
+    const unsigned char size = 13;
+    char buf[size];
+    size_t bytes_read = 0;
+    while(bytes_read != size)
+    {
+        const auto data = boost::asio::buffer(buf + bytes_read, size - bytes_read);
+        bytes_read = sock.read_some(data);
+    }
+    return std::string{buf, bytes_read};
+}
+
+
 int main()
 {
-    const unsigned short server_port = 3333;
+    const std::string address{"127.0.0.1"};
+    const short port = 3333;
 
-    const auto server_ip_address = boost::asio::ip::address_v6::any();
-    boost::asio::ip::tcp::endpoint endpoint{server_ip_address, server_port};
+    try
+    {
+        boost::asio::ip::tcp::endpoint ep{boost::asio::ip::address::from_string(address), port};
+        boost::asio::io_service ios;
+        boost::asio::ip::tcp::socket sock{ios, ep.protocol()};
+        sock.connect(ep);
+        const std::string data = read_from(sock);
+        std::cout << "Get: " << data << std::endl;
+    }
+    catch(const boost::system::system_error& ex)
+    {
+        std::cerr << "Error: " << ex.code() << ". " << ex.what() << std::endl;
+    }
 }
